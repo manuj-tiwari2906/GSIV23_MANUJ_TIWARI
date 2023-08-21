8c0049bc3e1a1bae892b4810b07d5c2f886d03cc
Manuj <manuka72tiwari@gmail.com>
2023-08-21 23:15:49 +0530

    updated-code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import store from "./store";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('ServiceWorker registration failed:', error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
